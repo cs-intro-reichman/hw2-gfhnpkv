316e600eed4ab20d9c8653952e275ab61292186c
daniel.benporat@post.runi.ac.il <gzb.dbp@gmail.com>
2025-11-04 22:31:11 +0200

    testing TimeCalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,31 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        // getting the inputs and converting them to hours and minitues
+        int hours = Integer.parseInt("" + args[0].charAt(0) +
+                                     args[0].charAt(1));
+        int minitues = Integer.parseInt("" + args[0].charAt(3) +
+                                     args[0].charAt(4));
+        int timeToAdd = Integer.parseInt(args[1]);
+    
+        //calculating how many houes and minitues to add
+        int totalHours = timeToAdd / 60;
+        int minituesToAdd = timeToAdd - (totalHours * 60);
+        while (minituesToAdd > 60) {
+            minituesToAdd = minituesToAdd - 60;
+            totalHours++;
+        }
+        int hoursToAdd = totalHours % 24;
+
+        //adding the time
+        minitues = minituesToAdd + minitues;
+        if (minitues >= 60) {
+            minitues = minitues - 60;
+            hours++;
+        }
+        hours = (hours + hoursToAdd) % 24;
+
+        //printing answer
+        System.out.println(hours + ":" + minitues);
+
     }
 }
