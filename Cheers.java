ed8f0e9e07fe6e9a8f6872f665fd4ce32b881c97
daniel.benporat@post.runi.ac.il <gzb.dbp@gmail.com>
2025-11-04 23:53:56 +0200

    Adding Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,26 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	        // Getting inputs
+                String word = args[0].toUpperCase();
+                int count = Integer.parseInt(args[1]);
+                
+                //printing the spelling
+                for (int i = 0; i < word.length(); i++) {
+                        String a = "a";
+                        String search = "AEFHILMNORSX";
+                        if (search.indexOf(word.charAt(i)) >= 0) {
+                                a = "an";
+                        }
+                        System.out.println("Give me " + a + " " +
+                                        word.charAt(i) + ": " + word.charAt(i) + "!");
+                }
+
+                //printing the word number of count
+                System.err.println("What does that spell?");
+                for (int i = 0; i < count; i++) {
+                        System.out.println(word + " ! ! !");
+            }
+
         }
 }
